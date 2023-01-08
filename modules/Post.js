aa9f60b987e27e1b42968be560dea47adfa1d8af
KingCheems69 <ahmadyasir4439@gmail.com>
2023-01-08 23:01:53 +0500

    error resolved.
    An return type error is resolved.

--- modules/Post.js
+++ modules/Post.js
@@ -16,7 +16,10 @@ router.post("/save", (req, res) => {
   let email = db.escape(req.body.Email)
   let pass = db.escape(req.body.Password)
 
-  let sql1 = `INSERT INTO users (email, password) VALUES (${email}, ${pass})`;
+  // Got the attributes from front end
+  // Check if a user exists with same email
+  let promiseOne = new Promise((resolve, reject) => {
+    let sql1 = `SELECT * FROM users WHERE email = ${email}`;
     let query1 = db.query(sql1, (err, result) => {
       if (err) {
         console.log(err);
@@ -24,19 +27,47 @@ router.post("/save", (req, res) => {
           message: "Some Error Occured in Checking Email",
         });
       }
-    })
+      if (result.length > 0) {
+        reject("Email Already exists.");
+        process.on('unhandledRejection', (reason, p) => {
+          console.error(reason);
+        });
+      }
+      resolve(result);
+    });
+  });
+  promiseOne.then(
+    (data) => {
+      // No Email Found With this Email
 
-  return res.json({
-    message: "data has been saved"
-  })
+      let sql2 = `INSERT INTO users (email, password) VALUES (${email}, ${pass})`;
+      let query2 = db.query(sql2, (err1, result) => {
+        if (err1) {
+          console.log(err1);
+          return res.status(500).json({
+            message: "Some Error Occured in Checking Email",
+          });
+        }
+        return res.status(200).json({
+          message: "Success",
+        });
+      });
+    },
+     (error) => {
+        // Email Found with given Email
+        return res.status(500).json({
+        message: error,
+        });
+      }
+      );
+    });
 
-})
+  router.post("/login", (req, res) => {
+    let email = db.escape(req.body.Email)
+    let pass = db.escape(req.body.Password)
 
-router.post("/login", (req, res) => {
-  let email = db.escape(req.body.Email)
-  let pass = db.escape(req.body.Password)
 
-  let sql1 = `SELECT * FROM users WHERE email = ${email} and password = ${pass}`;
+    let sql1 = `SELECT * FROM users WHERE email = ${email} and password = ${pass}`;
     let query1 = db.query(sql1, (err, result) => {
       if (err) {
         console.log(err);
@@ -45,14 +76,12 @@ router.post("/login", (req, res) => {
         });
       }
 
-      if (result.length > 0)
-      {
+      if (result.length > 0) {
         return res.status(200).json({
           valid: true
         })
       }
-      else
-      {
+      else {
         return res.status(200).json({
           valid: false
         })
@@ -60,7 +89,7 @@ router.post("/login", (req, res) => {
     })
 
 
-})
+  })
 
 
 module.exports = router;
